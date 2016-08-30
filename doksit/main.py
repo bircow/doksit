65384ed7c0c1c638620131f63319727af08ca93a
nait-naul <nait-aul@protonmail.com>
2016-08-30 23:20:08 +0200

    Added 'find_files' function to the 'doksit/main.py'.

--- doksit/main.py
+++ doksit/main.py
@@ -1,4 +1,7 @@
+import os
 import re
+import sys
+from typing import List, Tuple
 
 from doksit.utils import OrderedDict
 
@@ -7,11 +10,21 @@ method_regex = re.compile("^    def ([\w_]+)")
 function_regex = re.compile("^def ([\w_]+)")
 
 
-def read_file():
-    """Read the given file to find names of classes with its methods and
-    functions.
+def read_file(file_path: str) -> Tuple[OrderedDict, List[str]]:
+    """Get names for classes including methods inside and functions.
+
+    Unlike Pydoc the Doksit cares about order of objects specified above in the
+    given file + omits magic methods except the '__init__'.
+
+    Arguments:
+        file_path:
+            Relative file path.
+
+    Returns:
+        First item contains ordered dict with class names (key) and its
+        methods (value). Second items contains function names.
     """
-    path = "/home/nait-aul/Apps/doksit_env/tests/package/module.py"
+    path = os.getcwd() + "/" + file_path
 
     classes = OrderedDict()
     functions = []
@@ -37,5 +50,30 @@ def read_file():
     return classes, functions
 
 
-if __name__ == "__main__":
-    pass
+file_paths = []
+
+
+def find_files(directory_path: str) -> None:
+    """Get relative paths for all python files in the given directory and
+    subdirectories.
+
+    Files in '__pycache__' directories are excluded. The same goes for
+    '__init__.py' and '__main__.py'.
+
+    Arguments:
+        directory_path:
+            Relative directory path.
+    """
+    for entry in os.scandir(directory_path):
+        if entry.is_dir() and entry.name != "__pycache__":
+            find_files(entry.path)
+        elif entry.is_file() and entry.name.endswith(".py") \
+                and entry.name not in ["__init__.py", "__main__.py"]:
+            file_paths.append(entry.path)
+
+
+def main():
+    find_files(sys.argv[1])
+
+    for file in reversed(file_paths):
+        read_file(file)
