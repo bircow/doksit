b866d636bc19a34266cd0e0399b2c14d366419f7
nait-naul <nait-aul@protonmail.com>
2016-08-30 20:26:40 +0200

    Added test for 'read_file' function from 'main.py'.

---
+++ tests/test_reading_files.py
@@ -0,0 +1,58 @@
+import unittest
+
+from doksit.main import read_file, class_regex, method_regex, function_regex
+
+
+class TestReadFileFunction(unittest.TestCase):
+
+    def test_regex_for_classes(self):
+        classes = [
+            "class Foo:",
+            "class Foo(object):",
+            "class Foo(Bar, object):"
+        ]
+
+        for class_name in classes:
+            assert class_regex.search(class_name).group(1) == "Foo"
+
+    def test_regex_for_methods(self):
+        methods = [
+            "    def sample_method()",  # a static method
+            "    def sample_method(cls)",
+            "    def sample_method(cls, arg1, arg2, ...)",
+            "    def sample_method(self)",
+            "    def sample_method(self, arg1, arg2, ...)"
+        ]
+
+        for method_name in methods:
+            assert method_regex.search(method_name).group(1) == "sample_method"
+
+    def test_regex_for_functions(self):
+        functions = [
+            "def function_name()",
+            "def function_name(arg1, arg2, ...)"
+        ]
+
+        for function_name in functions:
+            assert function_regex.search(function_name).group(1) \
+                == "function_name"
+
+    def test_read_sample_file_from_sample_package(self):
+        """The file is located in 'tests/package/module.py'.
+
+        Expected result is:
+
+            (OrderedDict([('Foo', ['__init__', 'method']), ('Bar', [])]),
+            ['function', 'another_function'])
+
+        """
+        result = read_file()
+
+        assert list(result[0].keys()) == ["Foo", "Bar"]
+        assert result[0]["Foo"] == ["__init__", "method"]
+        assert not result[0]["Bar"]
+        assert result[1] == ["function", "another_function"]
+
+
+if __name__ == "__main__":
+    unittest.main()
