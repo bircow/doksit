4bf939f906405c5cafd19e47d26e2275b220f251
nait-naul <nait-aul@protonmail.com>
2016-08-31 15:41:31 +0200

    Added test for the 'find_files' function from the 'doksit.main'.

---
+++ tests/test_find_files.py
@@ -0,0 +1,24 @@
+import unittest
+
+from doksit.main import find_files, file_paths
+
+
+class TestFindFilesFunction(unittest.TestCase):
+
+    def test_find_files_in_the_given_directory(self):
+        """
+        The 'find_files' requires external (global) variable 'file_paths'
+        to run successfully.
+        """
+        expected_file_paths = [
+            "package/subpackage/subpackage/module.py",
+            "package/subpackage/module.py",
+            "package/module.py"
+        ]
+
+        find_files("package")
+        assert file_paths == expected_file_paths
+
+
+if __name__ == "__main__":
+    unittest.main()
