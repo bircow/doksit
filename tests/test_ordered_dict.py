d5f69048b366bceddc1561224945c92be8d3d273
nait-naul <nait-aul@protonmail.com>
2016-09-05 10:03:42 +0200

    Fixed import in the 'tests/test_ordered_dict.py'.

--- tests/test_ordered_dict.py
+++ tests/test_ordered_dict.py
@@ -1,12 +1,12 @@
 import unittest
 
-from doksit.utils import OrderedDict
+from doksit.utils import MyOrderedDict
 
 
 class TestModifiedOrderedDict(unittest.TestCase):
 
     def test_last_added_key(self):
-        sample_dict = OrderedDict()
+        sample_dict = MyOrderedDict()
         sample_dict.update({"foo": 0})
         sample_dict.update({"bar": 1})
 
