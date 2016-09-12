b8eb53bb2a93438f8af08c5cdbdfc8dd0ed6c220
nait-naul <nait-aul@protonmail.com>
2016-09-12 21:10:30 +0200

    Moved tests for modules in 'doksit.utils' directory to 'tests.utils' directory.

---
+++ tests/utils/test_parse_parameters.py
@@ -0,0 +1,49 @@
+import collections
+import inspect
+
+from doksit.utils.parser import parse_parameters
+
+from tests.test_data.module import Foo, function
+
+
+def test_parse_parameters_from_method():
+    """
+    Sample method 'Foo.__init__' has these parameters:
+
+    - self
+    - x: str
+    - y: float = 1.0
+    - z: List[int] = []
+
+    and expected (unorered) output is:
+
+    {"x": "x (str)", "y": "y (float, optional, default 1.0),
+     "z": "z (List[int], optional, [])}
+    """
+    method_parameters = inspect.signature(Foo.__init__).parameters
+    method_parameters = collections.OrderedDict(method_parameters)
+
+    output = parse_parameters(method_parameters)
+
+    assert "self" not in output
+    assert output["x"] == "x (str):"
+    assert output["y"] == "y (float, optional, default 1.0):"
+    assert output["z"] == "z (List[int], optional, default []):"
+
+
+def test_parse_parametrs_from_function():
+    """
+    Sample function 'function' has these paramaters:
+
+    - n
+
+    and expected output is:
+
+    {"n": "n:"}
+    """
+    function_parameters = inspect.signature(function).parameters
+    function_parameters = collections.OrderedDict(function_parameters)
+
+    output = parse_parameters(function_parameters)
+
+    assert output["n"] == "n:"
