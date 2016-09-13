cdb4158a35fca01372268c871776e003de2ac8fd
nait-naul <nait-aul@protonmail.com>
2016-09-13 22:17:10 +0200

    Rewritten rest of the tests for pytest.

--- tests/test_doksit.py
+++ tests/test_doksit.py
@@ -1,6 +1,9 @@
 import os
 
+import pytest
 
+
+@pytest.mark.usefixtures("check_directory")
 def test_doksit_command():
     os.system("python -m doksit test_data/ > tmp.md")
 
--- tests/test_get_documentation.py
+++ tests/test_get_documentation.py
@@ -1,19 +1,21 @@
-import unittest
+import sys
 
-from doksit.main import get_documentation, read_file
+from doksit.main import get_documentation, read_file, output
 
+# IMPORTANT:
+#
+# Add current directory to the Python sys path, othwerise this test fails.
+# I don't know why it's buggy. With normal script / Python interpret / unittest
+# it's working but with pytest no...
 
-class TestGetDocumentationFunction(unittest.TestCase):
-
-    def test_get_documentation_for_sample_file(self):
-        """
-        Sample file (module) is 'package.module'.
-        """
-        file_metadata = read_file("package/module.py")
-        file_documentation = get_documentation(file_metadata)
-
-        assert "package.module" in file_documentation
+sys.path.append(".")
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_get_documentation_for_sample_file():
+    """
+    Sample file (module) is 'package.module'.
+    """
+    file_metadata = read_file("test_data/module.py")
+    file_documentation = get_documentation(file_metadata)
+
+    assert "test_data.module" in file_documentation
