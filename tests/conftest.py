d95529daac3d9c12615db56ebdb0206760dbea9e
nait-naul <nait-aul@protonmail.com>
2016-09-13 22:19:36 +0200

    Added a fixture for checking current directory and switching if pytest command is run from the root of project and not from inside 'tests/'.

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import os
+import subprocess
+
+import pytest
+
+
+@pytest.fixture(scope="session")
+def check_directory():
+    """
+    Check if a user is inside 'tests' or not. If not, change the directory in
+    subprocess
+    """
+    if os.path.relpath(".", "..") != "tests":
+        os.chdir("tests/")
