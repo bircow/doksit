829d42cba49d16cc0de54f729e542ca4aace2e83
nait-naul <nait-aul@protonmail.com>
2016-09-20 01:31:03 +0200

    Moved from 'argparse' to external 'click' CLI library.

--- doksit/cli.py
+++ doksit/cli.py
@@ -1,14 +1,39 @@
-import argparse
+import click
 
-description = "Generate API Reference documentation"
+from doksit.api import find_files, get_documentation, read_file
 
-parser = argparse.ArgumentParser(description=description)
-parser.add_argument(
-    "-v", "--version",
-    action="version",
-    version="Doksit 0.1.0"
-)
-parser.add_argument(
-    "directory",
-    help="relative path to your Python package directory"
-)
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument("package_directory", type=click.Path(exists=True))
+def api(package_directory: str):
+    """
+    Command for generating the API Reference documentation for the given Python
+    package.
+
+    If a user doesn't use stdout redirection, then the documentation will be
+    printed like man pages.
+
+    Arguments:
+        package_dir:
+            Name of the Python package (relative path).
+    """
+    file_paths = find_files(package_directory)
+
+    api_documentation = ["# API Reference\n"]
+
+    for file in file_paths:
+        file_documentation = get_documentation(read_file(file))
+
+        if file_documentation is not None:
+            api_documentation.append(file_documentation)
+
+    # Remove 2 blank lines at the end of last item in the 'api_documentation'.
+
+    api_documentation[-1] = api_documentation[-1][:-2]
+    
+    click.echo_via_pager("\n".join(api_documentation))
