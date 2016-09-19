70ffe78d2a9f02d5a549aab5b33b1491353954a6
nait-naul <nait-aul@protonmail.com>
2016-09-19 23:02:24 +0200

    Updated 'tests.conftest'.

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,9 @@ import pytest
 @pytest.fixture(scope="session")
 def check_directory():
     """
-    Check if a user is inside 'tests' or not. If not, change the directory in
-    subprocess
+    Check if a user is inside the 'tests' directory or not. 
+
+    If not, change the directory to it for successful running tests.
     """
     if os.path.relpath(".", "..") != "tests":
         os.chdir("tests/")
