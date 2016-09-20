fc8e3f435d0ab5b47c4a30bb009bb6ac2585383a
nait-naul <nait-aul@protonmail.com>
2016-09-20 14:01:06 +0200

    Fixed doksit.utils.inspectors.get_line_numbers' function.

--- doksit/utils/inspectors.py
+++ doksit/utils/inspectors.py
@@ -1,25 +1,44 @@
 import inspect
 
+from typing import Any
 
-def get_line_numbers(object):
+
+def get_repository_link():
     """
-    Find on which lines is the object defined.
+    Get absolute URL path.
+    """
+    pass
+
+
+def get_line_numbers(object: Any):
+    """
+    Find on which lines is the given object defined.
 
     Arguments:
-        object (object):
+        object:
             For which object (class, method, function) to get the line numbers.
 
+    Note:
+        There is a problem for getting line numbers for defined CLI commands
+        using `click` package (will raise `TypeError). Therefore it will be
+        silenced and returned only `#`.
+
     Returns:
-        String with the object line numbers (range from-to).
+        String range, where the object definition starts and ends.
 
     Example:
         #L10-L25
     """
-    source_lines = inspect.getsourcelines(object)
+    try:
+        source_lines = inspect.getsourcelines(object)
+    except TypeError:
+        return "#"
+
     starting_line = source_lines[1]
     ending_line = starting_line + len(source_lines[0]) - 1
 
-    # Note: Without minues one for 'ending_line' it would include blank line
+    # Note: Without '- 1' for the 'ending_line' it would include blank line
     # after the object definition.
 
-    return "#L{0}-L{1}".format(starting_line, ending_line)
+    return "#L{starting_line}-L{ending_line}".format(
+        starting_line=starting_line, ending_line=ending_line)
