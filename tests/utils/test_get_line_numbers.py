aa8f618eba67188539a10998eb4ac2f7832bfc4d
nait-naul <nait-aul@protonmail.com>
2016-09-28 21:06:53 +0200

    Moved tests for private functions in the 'inspectors.py' to 'test_get_source_code_url.py'.

--- tests/utils/test_get_line_numbers.py
+++
@@ -1,8 +0,0 @@
-from doksit.utils.inspectors import _get_line_numbers
-
-from tests.test_data.module import Foo, function
-
-
-def test_get_object_line_numbers():
-    assert _get_line_numbers(Foo) == "#L12-L101"
-    assert _get_line_numbers(function) == "#L108-L126"
--- tests/utils/test_get_source_code_url.py
+++ tests/utils/test_get_source_code_url.py
@@ -1,6 +1,32 @@
-from doksit.utils.inspectors import get_source_code_url
+from doksit.utils.inspectors import (
+    REPOSITORY_URL_REGEX, BRANCH_NAME_REGEX, _get_repository_url,
+    _get_line_numbers, get_source_code_url)
 
 from tests.test_data import module
+from tests.test_data.module import Foo, function
+
+
+def test_repository_url_regex():
+    data = "origin\thttps://github.com/nait-aul/doksit (fetch)\n" \
+        "origin\thttps://github.com/nait-aul/doksit (push)\n"
+    expected_output = "https://github.com/nait-aul/doksit"
+
+    assert REPOSITORY_URL_REGEX.search(data).group(1) == expected_output
+
+
+def test_branch_name_regex():
+    data = "  master\n* dev\n  nait"
+
+    assert BRANCH_NAME_REGEX.search(data).group(1) == "dev"
+
+
+def test_get_url_to_github_repository():
+    assert "doksit/" in _get_repository_url()
+
+
+def test_get_object_line_numbers():
+    assert _get_line_numbers(Foo) == "#L12-L101"
+    assert _get_line_numbers(function) == "#L108-L126"
 
 
 def test_get_source_code_url():
