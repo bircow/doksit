3ba54d054d833196a06a05b26425a175b5966f90
nait-naul <nait-aul@protonmail.com>
2016-10-12 22:12:08 +0200

    Updated docstring for the 'PackageError'.

--- doksit/exceptions.py
+++ doksit/exceptions.py
@@ -2,7 +2,11 @@
 Here are defined exceptions.
 """
 
+
 class PackageError(Exception):
+    """
+    When Doksit cannot find a package directory.
+    """
 
     def __str__(self):
         return "Cannost guess a package, please use option:\n'-p <package>'"
