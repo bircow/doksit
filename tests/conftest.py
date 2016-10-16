af40aa68b34892938e6e1fa4b4f621e81d19311f
nait-naul <nait-aul@protonmail.com>
2016-10-17 00:18:23 +0200

    Added fixture 'change_temporarily_directory'.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,20 @@ import pytest
 
 
 @pytest.fixture(scope="session")
-def check_directory():
+def check_test_directory():
     """
-    If a user will run tests from project root directory, then change directory
+    If a user run tests from project root directory, then change directory
     to 'tests' for successful running.
     """
     if os.path.relpath(".", "..") != "tests" and "setup.py" in os.listdir():
         os.chdir("tests/")
+
+
+@pytest.fixture
+def change_temporarily_directory():
+    current_directory = os.getcwd()
+    os.chdir("../..")
+
+    yield
+
+    os.chdir(current_directory)
