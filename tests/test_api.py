ceef1ac41024899bbd5182aeaf934a612e9da076
nait-naul <nait-aul@protonmail.com>
2016-10-19 16:41:54 +0200

    Added missing test for '--smooth' flag.

--- tests/test_api.py
+++ tests/test_api.py
@@ -29,7 +29,13 @@ def test_title_option():
     assert "# Different title" in result.output
 
 
-def test_api_subcommand_with_colored_option():
+def test_api_subcommand_with_smooth_flag():
+    result = RUNNER.invoke(api, ["-p", "test_data/", "--smooth"])
+
+    assert result.exit_code == 0
+
+
+def test_api_subcommand_with_colored_flag():
     """
     Don't assert a content in the result.output, because it executes
     subprocess call.
