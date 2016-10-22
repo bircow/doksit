46cc81dcc193006108da901fb53f6ad9df66e842
nait-naul <nait-aul@protonmail.com>
2016-10-22 23:13:34 +0200

    Added 'InvalidObject' exception.

--- doksit/exceptions.py
+++ doksit/exceptions.py
@@ -1,6 +1,25 @@
 """
-Here are defined exceptions.
+Here are defined own exceptions.
 """
+
+
+class InvalidObject(Exception):
+    """
+    When a user placed into a module docstring a template variable with
+    invalid object reference (to a class / funciton).
+    """
+    __slots__ = ("module", "object_name")
+
+    def __init__(self, module_name: str, object_name: str):
+        super().__init__()
+        self.module_name = module_name
+        self.object_name = object_name
+
+    def __str__(self):
+        return (
+            "In a {module}'s docstring is an invalid template variable "
+            "'{{{{ {variable} }}}}'."
+        ).format(module=self.module_name, variable=self.object_name)
 
 
 class PackageError(Exception):
