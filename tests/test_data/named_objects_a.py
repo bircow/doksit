0b5e9f76cbfc318cda63d6aa8bb477a56599ba79
nait-naul <nait-aul@protonmail.com>
2016-10-22 23:14:49 +0200

    Added another modules to 'tests.test_data'.

---
+++ tests/test_data/named_objects_a.py
@@ -0,0 +1,48 @@
+"""
+# Blabla
+
+This is a module docstring.
+
+{{ Foo }}
+{{ function }}
+"""
+
+
+class Foo:
+    """
+    Foo class docstring.
+    """
+
+    def method(self):
+        """
+        Foo.method docstring.
+        """
+        pass
+
+
+class Bar:
+    """
+    Bar class docstring.
+    """
+    pass
+
+    @staticmethod
+    def static_method():
+        """
+        Bar.static_method docstring.
+        """
+        pass
+
+
+def function():
+    """
+    Function function docstring.
+    """
+    pass
+
+
+def another_function():
+    """
+    Function another_function docstring.
+    """
+    pass
---
+++ tests/test_data/named_objects_b.py
@@ -0,0 +1,12 @@
+"""
+Module docstring using a variable template, but without set module title.
+
+{{ function }}
+"""
+
+
+def function():
+    """
+    Function function docstring.
+    """
+    pass
